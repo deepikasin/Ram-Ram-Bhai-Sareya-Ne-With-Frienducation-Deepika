80f280ff4e9409f588789ef506922ad4429913c2
deepikasin <122626294+deepikasin@users.noreply.github.com>
2024-01-06 17:33:23 +0530

    Add files via upload

---
+++ dublicate.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int removeDuplicates(vector<int>& arr) {
+        if(arr.size()==1){
+            return 1;
+        }
+        int u=1;
+        for(int i=1;i<arr.size();i++){
+            if(arr[i]!=arr[i-1]){
+                arr[u++]=arr[i];
+            }
+        }
+        return u;
+    }
+};
