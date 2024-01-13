4a17ee5511aae7c15794f802fcb805a3d1e9cf12
deepikasin <122626294+deepikasin@users.noreply.github.com>
2024-01-13 18:12:15 +0530

    Day-8

---
+++ first_last.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    bool solve(string &s,int i,int j){
+        while(i<j){
+            if(s[i]!=s[j]){
+                return false;
+            }
+            i++;
+            j--;
+        }
+        return true;
+    }
+    string longestPalindrome(string s) {
+        int n=s.size();
+        int st=0;
+        int ans=0;
+        for(int i=0;i<n;i++){
+            for(int j=i;j<n;j++){
+                if(solve(s,i,j)){
+                    if(j-i+1 > ans ){
+                        ans=j-i+1;
+                        st=i;
+                    }
+                }
+            }
+        }
+        return s.substr(st,ans);
+    }
+};
+//O(N^2)
